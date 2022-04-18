2b4b508a717302f8fa0bea156c923395c75ba286
OngWeiLun <93892957+OngWeiLun@users.noreply.github.com>
2022-04-18 21:20:53 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,6 +11,14 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter height(m) and weight(kg)");
     double height = in.nextDouble();
+    double weight = in.nextDouble();
+    
+    double bmi = weight/(height*height);
+    
+    System.out.println("BMI: " + bmi);
+    
+    in.close();
   }
 }
